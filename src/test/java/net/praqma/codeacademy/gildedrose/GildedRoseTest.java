b2ed2f44a9199e5ce9f97318294abab6dd1fd41b
gatikman <aga@reviso.com>
2017-08-08 13:11:43 +0200

    Update GildedRoseTest.java

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-/*
+
+    
+   //Test
+    /*
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
